b6c3c64aeb940c0619bb1b177215a5325ee4dfd5
Edwin C George <edwincgeorge-work@outlook.com>
2026-03-29 03:41:03 -0700

    Update db.py

--- db.py
+++ db.py
@@ -6,6 +6,8 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
+            timeout=10,
+            check_same_thread=False
         )
         g.db.row_factory = sqlite3.Row
     return g.db
